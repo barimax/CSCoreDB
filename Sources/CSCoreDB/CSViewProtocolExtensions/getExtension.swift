13bba0069522f9492bfc66cd05d013c36f5e2c25
Georgie Ivanov <georgie@barimax.bg>
2019-08-19 15:51:34 +0300

    make get methods public

--- Sources/CSCoreDB/CSViewProtocolExtensions/getExtension.swift
+++ Sources/CSCoreDB/CSViewProtocolExtensions/getExtension.swift
@@ -7,10 +7,10 @@
 import PerfectCRUD
 
 extension CSViewProtocol {
-    func getAll() throws -> [Entity] {
+    public func getAll() throws -> [Entity] {
         return try table.select().map { $0 }
     }
-    func get(id: Int) throws -> Entity {
+    public func get(id: Int) throws -> Entity {
         guard let entity = try table.where(\Entity.id == id).first() else {
             throw CSCoreDBError.entityNotFound
         }
